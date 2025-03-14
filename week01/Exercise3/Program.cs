aa20dc0461ec18f7465b47c70c0317914a987134
JeffBen4 <benjaminjeffreycoilacanaza@gmail.com>
2025-03-14 18:38:14 -0500

    ola

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string response;
+        do
+        {
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+
+        } while (response == "yes");
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+        
+
     }
 }
