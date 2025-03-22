460098f26669bcdc5637f84efed7e0f833e6a335
JeffBen4 <benjaminjeffreycoilacanaza@gmail.com>
2025-03-22 16:09:08 -0500

    updated

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Welcome to the program!");
+        
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+        
+        int squaredNumber = favoriteNumber * favoriteNumber;
+        
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
